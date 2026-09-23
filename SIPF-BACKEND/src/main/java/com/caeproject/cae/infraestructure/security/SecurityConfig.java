22c9f6fefdfcd8f7275b4082dcd2f7a6fc25d88d
Zerriug <fadeslol222@gmail.com>
2026-09-23 14:15:19 -0500

    Fix: Permitir ruta /error en SecurityConfig para ver errores reales de correo

--- SIPF-BACKEND/src/main/java/com/caeproject/cae/infraestructure/security/SecurityConfig.java
+++ SIPF-BACKEND/src/main/java/com/caeproject/cae/infraestructure/security/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/api/auth/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/auth/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/usuarios").permitAll()
-
+                        .requestMatchers("/error").permitAll()
                         .requestMatchers("/h2-console/**").permitAll() //MOMENTANEO H2
                         .requestMatchers("/api/programas/**").hasAnyRole("COORDINADOR", "ADMINISTRADOR")
                         .requestMatchers("/api/fichas/**").hasAnyRole("COORDINADOR", "ADMINISTRADOR")
